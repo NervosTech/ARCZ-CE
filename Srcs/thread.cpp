bbcf7b5ccda4ff401966209b00a3b44de0cba165
Mohamed Nayeem <a.mohamednayeem@gmail.com>
2016-05-18 16:41:53 +0400

    Small Correction

--- Srcs/thread.cpp
+++ Srcs/thread.cpp
@@ -1,19 +1,16 @@
 /*
-  Nayeem , a UCI chess playing engine derived from Stockfish
-  Copyright (C) 2008-2015 Marco Costalba, Joona Kiiski, Tord Romstad
-
-  Nayeem  is free software: you can redistribute it and/or modify
+  Nayeem - A UCI chess engine derived from Stockfish.
+  Copyright (C) 2016 Mohamed Nayeem
+  Nayeem is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-
-  Nayeem  is distributed in the hope that it will be useful,
+  Nayeem is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-
   You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  along with Nayeem. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <algorithm> // For std::count
