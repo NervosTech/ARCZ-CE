ab61fe411d2c39c1da2bd12a685246dd950471ef
Mohamed Nayeem <a.mohamednayeem@gmail.com>
2016-06-22 21:21:43 +0400

    Add files via upload

--- Srcs/search.cpp
+++ Srcs/search.cpp
@@ -1147,7 +1147,7 @@ moves_loop: // When in check search starts from here
 
           // Increase reduction for cut nodes
           if (!PvNode && cutNode)
-              r += ONE_PLY;
+              r += 2 * ONE_PLY;
 
           if (PvNode && extension)
 			  r = std::max(DEPTH_ZERO, r - ONE_PLY);
@@ -1159,7 +1159,7 @@ moves_loop: // When in check search starts from here
           else if (   type_of(move) == NORMAL
 		           && type_of(pos.piece_on(to_sq(move))) != PAWN
 				   && pos.see(make_move(to_sq(move), from_sq(move))) < VALUE_ZERO)
-		      r -= ONE_PLY;
+		      r -= 2 * ONE_PLY;
 			  
 		  // Decrease/increase reduction for moves with a good/bad history
 		  int rHist = (val - 10000) / 20000;
